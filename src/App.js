65e247b5d23408d6c03890357e586c3ecc149b78
Stanley Chen <dannyshien@gmail.com>
2019-09-25 14:28:06 -0400

    Accidenlty working on master, add order to numeric-keys and beginnings of state

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
 import React from 'react';
-import './App.css';
+import Calculator from './components/Calculator.js'
 
-function App() {
+const App = () => {
     return (
-        <div className="App">    
-            <div className='calculator'>
-                <div className='calculator-display'>0</div>
-                <div className='calculator-keypad'>
-                    <div className='key-set1'>
-                        <div className='function-keys'>
-                            <button className='keys'>A/C</button>
-                            <button className='keys'>±</button>
-                            <button className='keys'>%</button>
-                        </div>
-                        <div className='numeric-keys'>
-                            <button className='keys'>9</button>
-                            <button className='keys'>8</button>
-                            <button className='keys'>7</button>
-                            <button className='keys'>6</button>
-                            <button className='keys'>5</button>
-                            <button className='keys'>4</button>
-                            <button className='keys'>3</button>
-                            <button className='keys'>2</button>
-                            <button className='keys'>1</button>
-                            <button className='keys key-0'>0</button>
-                            <button className='keys'>•</button>
-                        </div>
-                    </div>
-                    <div className='key-set2'> 
-                        <div className='operator-keys'>
-                            <button className='keys'>÷</button>
-                            <button className='keys'>*</button>
-                            <button className='keys'>-</button>
-                            <button className='keys'>+</button>
-                            <button className='keys'>=</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+        <Calculator />
+    )
 }
 
 export default App;
