8bd2ecf1d7f6f246ae0119c23b9c85bddcc909b5
Stanley Chen <dannyshien@gmail.com>
2019-09-26 14:50:03 -0400

    set event handler to set state for each numeric-key pressed

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react'
 import './Calculator.css'
 
 class Calculator extends Component {
-    state = {
-        displayValue: 0
-    }
-    // constructor(props) {
-    //     super(props); 
-    //     this.state = {
-    //         displayValue: 0
-    //     }
+    // state = {
+    //     displayValue: 0
     // }
+    constructor(props) {
+        super(props); 
+        this.state = {
+            displayValue: 0
+        }
+    }
 
+    inputValue(digit) {
+        const {displayValue} = this.state
+        
+        this.setState({
+            displayValue: (digit)
+        })
+    }
 
     render() {
         const {displayValue} = this.state
@@ -27,17 +34,17 @@ class Calculator extends Component {
                             <button className='calculator-keys'>%</button>
                         </div>
                         <div className='numeric-keys'>
-                            <button className='calculator-keys key-1'>1</button>
-                            <button className='calculator-keys key-2'>2</button>
-                            <button className='calculator-keys key-3'>3</button>
-                            <button className='calculator-keys key-4'>4</button>
-                            <button className='calculator-keys key-5'>5</button>
-                            <button className='calculator-keys key-6'>6</button>
-                            <button className='calculator-keys key-7'>7</button>
-                            <button className='calculator-keys key-8'>8</button>
-                            <button className='calculator-keys key-9'>9</button>
-                            <button className='calculator-keys key-0'>0</button>
-                            <button className='calculator-keys key-•'>•</button>
+                            <button className='calculator-keys key-1' onClick={() => this.inputValue(1)}>1</button>
+                            <button className='calculator-keys key-2' onClick={() => this.inputValue(2)}>2</button>
+                            <button className='calculator-keys key-3' onClick={() => this.inputValue(3)}>3</button>
+                            <button className='calculator-keys key-4' onClick={() => this.inputValue(4)}>4</button>
+                            <button className='calculator-keys key-5' onClick={() => this.inputValue(5)}>5</button>
+                            <button className='calculator-keys key-6' onClick={() => this.inputValue(6)}>6</button>
+                            <button className='calculator-keys key-7' onClick={() => this.inputValue(7)}>7</button>
+                            <button className='calculator-keys key-8' onClick={() => this.inputValue(8)}>8</button>
+                            <button className='calculator-keys key-9' onClick={() => this.inputValue(9)}>9</button>
+                            <button className='calculator-keys key-0' onClick={() => this.inputValue(0)}>0</button>
+                            <button className='calculator-keys key-•' onClick={() => this.inputValue('.')}>•</button>
                         </div>
                     </div>
                     <div className='key-set2'> 
